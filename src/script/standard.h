7fc5e5d793cb93e50e5259efbd94eff5bdfef881
Sean Bowe <ewillbefull@gmail.com>
2017-03-01 06:00:34 -0700

    GetScriptForHTLC helper function.

--- src/script/standard.h
+++ src/script/standard.h
@@ -161,6 +161,13 @@ CScript GetScriptForRawPubKey(const CPubKey& pubkey);
 /** Generate a multisig script. */
 CScript GetScriptForMultisig(int nRequired, const std::vector<CPubKey>& keys);
 
+CScript GetScriptForHTLC(const CPubKey& seller,
+                         const CPubKey& refund,
+                         const std::vector<unsigned char> image,
+                         uint32_t timeout,
+                         opcodetype hasher_type,
+                         opcodetype timeout_type);
+
 /**
  * Generate a pay-to-witness script for the given redeem script. If the redeem
  * script is P2PK or P2PKH, this returns a P2WPKH script, otherwise it returns a
